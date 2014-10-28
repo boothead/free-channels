ea2f910e4b62d77528b0eb461a9a6995b22cf2fc
Ben Ford <ben@perurbis.com>
2014-10-28 08:54:59 +0000

    Existentially qualify the type of channel operations
    This is so we can have channels of different types in the same operation.

--- Channel.hs
+++ Channel.hs
@@ -1,9 +1,10 @@
 {-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE ExistentialQuantification #-}
 
 module Channel where
 
--- import           Control.Applicative
+import           Control.Applicative
 import           Control.Monad.IO.Class (MonadIO, liftIO)
 import           Control.Monad.Trans.Free
 import           Data.Foldable (forM_)
@@ -40,41 +41,52 @@ putQ q a = do
 
 --------------------------------------------------------------------------------
 
-data ChannelF a next = Put (Chan a) a next
-                     | Recv (Chan a) (Maybe a -> next)
-                     | MkChan (Chan a -> next)
-     deriving (Functor)
-     
-type Channel a m = FreeT (ChannelF a) m
+data ChannelF next = forall a. Put (Chan a) a next
+                   | forall a. Recv (Chan a) (Maybe a -> next)
+                   | forall a. MkChan (Chan a -> next)
 
-putC :: MonadFree (ChannelF a) m => Chan a -> a -> m ()
+instance Functor ChannelF where
+  fmap f (Put chan a next) = Put chan a (f next)
+  fmap f (Recv chan next)  = Recv chan (f . next)
+  fmap f (MkChan next)     = MkChan (f . next)
+     
+type Channel m = FreeT ChannelF m
+
+putC :: MonadFree ChannelF m => Chan a -> a -> m ()
 putC chan a = liftF $ Put chan a ()
 
-recvC :: MonadFree (ChannelF a) m => Chan a -> m (Maybe a)
+recvC :: MonadFree ChannelF m => Chan a -> m (Maybe a)
 recvC chan = liftF $ Recv chan id
 
-mkChan :: MonadFree (ChannelF a) m => m (Chan a)
+mkChan :: MonadFree ChannelF m => m (Chan a)
 mkChan = liftF $ MkChan id
 
 
 --------------------------------------------------------------------------------
 
-prog :: (MonadIO m) => Channel Int m String
+prog :: (MonadIO m) => Channel m (Maybe (Int, String))
 prog = do
   chan <- mkChan
-  putC chan 1
+  chan2 <- mkChan
+  putC chan (1 :: Int)
   el <- recvC chan
   el2 <- recvC chan
   liftIO $ print (el, el2)
-  forM_ [5..10] $ putC chan
-  sequence_ $ replicate 3 $ do
-     e <- recvC chan
-     liftIO $ print e
-  return $ show (el, el2)
+  forM_ [5..10 :: Int] $ (putC chan2) . show
+  sequence_ $ replicate 6 $ do
+     e <- recvC chan2
+     case e of
+       Nothing -> return ()
+       Just n -> liftIO $ putStrLn ("Chan2 got: " ++ n)
+  putC chan 10
+  putC chan2 "Bye"
+  eInt <- recvC chan
+  eStr <- recvC chan2
+  return $ (,) <$> eInt <*> eStr
 
 --------------------------------------------------------------------------------
 
-interpret :: MonadIO m => FreeT (ChannelF a) m b -> m b
+interpret :: MonadIO m => FreeT ChannelF m b -> m b
 interpret chan' = do
   c <- runFreeT chan'
   case c of
@@ -90,5 +102,5 @@ interpret chan' = do
     (Free (Recv chan nxt)) -> do
       interpret $ nxt =<< liftIO (getChan chan)
       
-test :: IO String
+test :: IO (Maybe (Int, String))
 test = interpret prog
